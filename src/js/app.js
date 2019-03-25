81943ba697998dabb4077e0599b45916055b8fde
reddingk <kris.redding3@gmail.com>
2019-03-25 19:49:11 -0400

    init

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+/* Components */
+
+/* Styles */
+import "../css/app.less";
+
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render(){     
+        return(
+            <div></div>
+        );
+    }
+
+    componentDidMount(){}
+}
+
+export default App;
