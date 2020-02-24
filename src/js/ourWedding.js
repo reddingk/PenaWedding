e550a2f21522b6de9f85f41ac1f4ff01953444e0
reddingk <kris.redding3@gmail.com>
2020-02-24 09:49:37 -0500

    update ui

--- src/js/ourWedding.js
+++ src/js/ourWedding.js
@@ -74,7 +74,13 @@ class OurWedding extends Component{
                                 <p>5:00pm</p>
 
                                 <p>Reception</p>
-                                <p>6:00 PM</p>                                
+                                <p>6:00 PM</p>  
+
+                                <h2>Hotel Information</h2>
+                                <div className="rsvp-message">For our guest from outside the area we have special hotel rates at <b>La Quinta Inn & Suites by Wyndham Salem NH</b></div>
+                                <div className="btn-container">
+                                    <a href="https://www.wyndhamhotels.com/laquinta/salem-new-hampshire/la-quinta-salem-nh/rooms-rates?&checkInDate=04/26/2020&checkOutDate=04/28/2020&groupCode=CGMEPE" target="_blank" className="btn light wide">Reserve A Room</a>                              
+                                </div>                                  
                             </div>
                         </div>
                     </div>
